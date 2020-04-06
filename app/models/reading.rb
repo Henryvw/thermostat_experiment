d20b9bd94c320939bb443d330794199063f167f4
Henry van Wagenberg <henryvw1@gmail.com>
2020-04-06 12:58:44 +0200

    Add stats for readings

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -1,4 +1,32 @@
 class Reading < ApplicationRecord
   belongs_to :thermostat
-  acts_as_sequenced scope: :thermostat_id
+  #acts_as_sequenced scope: :thermostat_id
+
+  def self.time_frame(start, stop)
+    Reading.where(created_at: start..stop)
+  end
+
+  def self.find_min(scientific_measurement)
+    Reading.minimum(scientific_measurement.to_sym)
+  end
+
+  def self.find_max(scientific_measurement)
+    Reading.maximum(scientific_measurement.to_sym)
+  end
+
+  def self.find_average(scientific_measurement)
+    Reading.average(scientific_measurement.to_sym)
+  end
+
+  def self.find_all_measurements
+    measurement_types = ["temperature", "humidity", "battery_charge"]
+    stats = {}
+    measurement_types.each do |measurement|
+      stats["min_" + measurement] = Reading.find_min(measurement)
+      stats["max_" + measurement] = Reading.find_max(measurement)
+      stats["avg_" + measurement] = Reading.find_average(measurement)
+    end
+
+    stats
+  end
 end
--- app/models/thermostat.rb
+++ app/models/thermostat.rb
@@ -1,2 +1,8 @@
 class Thermostat < ApplicationRecord
+  has_many :readings
+
+  def assemble_stats(start, stop)
+    households_readings = self.readings
+    all_stats_in_timeframe = households_readings.time_frame(start, stop).find_all_measurements
+  end
 end
