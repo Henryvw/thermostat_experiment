9b94233f7d39d54fdfb6facd0318a7085935bff4
Henry van Wagenberg <henryvw1@gmail.com>
2020-04-12 18:27:01 +0200

    Comment out seed database since Im now using factory

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,6 +32,7 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -61,7 +62,8 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   
-  config.before(:suite) do
-    Rails.application.load_seed
-  end
+#  config.before(:suite) do
+#    Rails.application.load_seed
+#  end
+  config.include FactoryBot::Syntax::Methods
 end
